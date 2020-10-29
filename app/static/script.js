07ae49e19da0c13687cefcee2ead0ea0fef36685
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-10-29 18:53:03 +0100

    ⚗ Added js file and testing DOM

---
+++ app/static/script.js
@@ -0,0 +1,6 @@
+// JavaScript file
+
+//Testing DOM
+// let user_input = getElementById('user_input');
+let user_input = prompt('Entre ta question');
+document.getElementById('user_question').innerHTML = user_input;
