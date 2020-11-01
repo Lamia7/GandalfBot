03ee5fe1590338926eba930f7736e407ca828d29
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-11-01 22:36:21 +0100

    ⚗ Sends user_input to user_question on chatbox

--- app/static/script.js
+++ app/static/script.js
@@ -1,6 +1,25 @@
 // JavaScript file
 
-//Testing DOM
-// let user_input = getElementById('user_input');
-let user_input = prompt('Entre ta question');
-document.getElementById('user_question').innerHTML = user_input;
+// QUERY SELECTOR
+document.querySelector('.send_btn').addEventListener('click', function (event) {
+    event.preventDefault();  // avoid default behavior that sends all the form once we click on button
+    const user_input = document.querySelector('#user_input').value; // gets the value of the input that user enters
+    console.log(user_input); // prints user input on console
+    document.querySelector('#user_question').textContent = user_input; // updates the user_question part with the value entered by user on user_input
+});
+
+console.log("fin");
+
+
+// GET ELEMENTS
+/*document.getElementsByClassName('send_btn').addEventListener('click', function () {
+    const user_input = document.getElementById('user_input').value;
+    console.log(user_input);
+});
+console.log("fin");*/
+
+
+
+
+
+
