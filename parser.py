3712400b26cd34aee555aeef91cca2eaafbac1ee
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-11-11 19:29:55 +0100

    🌱 Starting parser module that will treat data

---
+++ parser.py
@@ -0,0 +1,26 @@
+"""Module that represents the parser that will analyze the question asked by
+1) cutting down data (question) in words
+2) analyzing words
+3) keeping the key words
+"""
+
+
+class Parser:
+
+    def __init__(self):
+        pass
+
+    def cut_question(self):
+        pass
+
+    def analyze_words(self):
+        pass
+
+    def delete_stop_words(self):
+        pass
+
+    def delete_accents(self):
+        pass
+
+    def make_lower_case(self):
+        pass
