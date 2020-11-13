9928abe919dbf0a61aa350d9c6f2bc40e8f72084
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-11-13 17:44:49 +0100

    ⚗ Testing parser methods 'lowercase' and 'delete accents'

--- parser.py
+++ parser.py
@@ -2,7 +2,25 @@
 1) cutting down data (question) in words
 2) analyzing words
 3) keeping the key words
+
+- normaliser minuscules/majuscules
+- éliminer accents
+- isoler lieu
+- éliminer stop words
 """
+from unidecode import unidecode
+
+# testing delete_accents method with unidecode library
+test = "voilà l'été !"
+test_sans_accent = unidecode(test)
+print(test)
+print(test_sans_accent)
+
+#testing make_lower_case method
+phrase = "Bonjour Lamia"
+phrase_lower = phrase.lower()
+print(phrase)
+print(phrase_lower)
 
 
 class Parser:
@@ -19,8 +37,8 @@ class Parser:
     def delete_stop_words(self):
         pass
 
-    def delete_accents(self):
-        pass
+    def delete_accents(self, user_input):
+        return unidecode(user_input)
 
-    def make_lower_case(self):
-        pass
+    def make_lower_case(self, user_input):
+        return user_input.lower()
