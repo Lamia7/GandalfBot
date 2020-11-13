7825a6842dbcb16f2325834557aca83455f4352c
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-11-13 15:50:39 +0100

    🎨 Cleaning and added form reset after submit

--- app/static/script.js
+++ app/static/script.js
@@ -9,13 +9,12 @@ function sendInput(message) {
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify(message));
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) { // 'this' represents 'xhr'
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) { // or 'this.' to represent 'xhr'
             let response = JSON.parse(xhr.responseText) // parsing text response into JSON
-            document.querySelector('#bot_reply').textContent = response; // displays bot_reply to chatbox
-            //console.log(`Réponse côté JS POSTresponse:  ${response}`)
-            //return response
+             // displays bot_reply to chatbox
+            document.querySelector('#bot_reply').textContent = response;
         } else {
-        console.log('pas ok');
+        console.log('Il y a eu un problème.');
         }
     }
 };
@@ -23,62 +22,11 @@ function sendInput(message) {
 // Create variables with data get from HTML
 let form = document.querySelector('.questions');
 
-    // Sends the user_input value to the server and chatbox when capture a submit event on form
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();  // avoid default behavior that sends all the form once we click on button
-        let input = document.querySelector('#user_input').value; // gets the value of the input that user enters
-        document.querySelector('#user_question').textContent = input; // displays user_input to chatbox
-        sendInput(input); // calls sendInput function and send data to server
-    })
-
-
-/* APRES EVENT.PREVENTDEFAULT() LIGNE 8
-let user_input = document.querySelector('#user_input').value; // gets the value of the input that user enters
-console.log(user_input); // prints user input on console
-document.querySelector('#user_question').textContent = user_input;
-*/
-
-
-// GET ELEMENTS
-/*document.getElementsByClassName('send_btn').addEventListener('click', function () {
-    const user_input = document.getElementById('user_input').value;
-    console.log(user_input);
-});
-form.reset(); // clears form after sumbitting
-console.log("fin");
-
-
-
-
-/*
--------------
-// What to do when submit form (make it a function later)
-form.addEventListener('submit', function (event) {
+// Sends the user_input value to the server and chatbox when capture a submit event on form
+form.addEventListener('submit', function(event) {
     event.preventDefault();  // avoid default behavior that sends all the form once we click on button
-    console.log(user_input); // prints user input on console
-    document.querySelector('#user_question').textContent = user_input;
-
-
-    // Sends user_input content to the server
-    fetch('/result', {
-        method: "POST",
-        body: document.querySelector('#user_input').value // data that I am sending
-    });
-
+    let input = document.querySelector('#user_input').value; // gets the value of the input that user enters
+    document.querySelector('#user_question').textContent = input; // displays user_input to chatbox
+    sendInput(input); // calls sendInput function and send data to server
+    form.reset(); // clears form after sumbitting
 })
----------------
-
-// QUERY SELECTOR
-let send_btn = document.querySelector('.send_btn')
-send_btn.addEventListener('click', function (event) {
-    event.preventDefault();  // avoid default behavior that sends all the form once we click on button
-    const user_input = document.querySelector('#user_input').value; // gets the value of the input that user enters
-    console.log(user_input); // prints user input on console
-    document.querySelector('#user_question').textContent = user_input;*/ // updates the user_question part with the value entered by user on user_input
-
-
-
-
-
-
-
