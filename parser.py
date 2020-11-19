432293a5eeaecde09990aa4b00c499e0cb0b7fa4
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-11-19 13:15:31 +0100

    ✨ parser: remove punctuation

--- parser.py
+++ parser.py
@@ -10,25 +10,29 @@
 """
 from unidecode import unidecode
 
-# testing delete_accents method with unidecode library
-test = "voilà l'été !"
-test_sans_accent = unidecode(test)
-print(test)
-print(test_sans_accent)
-
-#testing make_lower_case method
-phrase = "Bonjour Lamia"
-phrase_lower = phrase.lower()
-print(phrase)
-print(phrase_lower)
-
 
 class Parser:
 
     def __init__(self):
         pass
 
-    def cut_question(self):
+    def delete_accents(self, user_input):
+        return unidecode(user_input)
+
+    def make_lower_case(self, user_input):
+        return user_input.lower()
+
+    def delete_punctuation(self, user_input):
+        """Removes punctuation from user input"""
+        punctuation = ["'", '"', ",", ".", "!", ":", ";", "?"]
+
+        for char in user_input:
+            if char in punctuation:
+                user_input = user_input.replace(char, " ")
+        return user_input
+
+    def cut_question(self, user_input):
+        """words_list = user_input.split()"""
         pass
 
     def analyze_words(self):
@@ -37,8 +41,26 @@ class Parser:
     def delete_stop_words(self):
         pass
 
-    def delete_accents(self, user_input):
-        return unidecode(user_input)
 
-    def make_lower_case(self, user_input):
-        return user_input.lower()
+"""
+# testing delete_accents method with unidecode library
+test = "voilà l'été !"
+test_sans_accent = unidecode(test)
+print(test)
+print(test_sans_accent)
+
+# testing make_lower_case method
+phrase = "Bonjour Lamia"
+phrase_lower = phrase.lower()
+print(phrase)
+print(phrase_lower)
+
+# testing delete_punctuation method
+phrase = "Bonjour ! Comment ça va ?"
+phrase2 = "Bonjour! Comment ça va ?"
+
+phrase_no_punc = delete_punctuation(phrase)
+phrase_no_punc2 = delete_punctuation(phrase2)
+print(f"Phrase 1: {phrase_no_punc} \n"
+      f"Phrase 2: {phrase_no_punc2}")
+"""
