16c553214884684af2f9d455f480080739f3c047
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-11-20 18:21:54 +0100

    💩 Testing wiki api class

--- app/wiki_api.py
+++ app/wiki_api.py
@@ -6,49 +6,69 @@ Module test pour la récupération des données de l'API wikimedia
 
 import requests
 
-from config import WIKI_API_URL, PAYLOAD
+from config import WIKI_API_URL
 
 
 class Wikiwrapper:
 
-    def __init__(self):
+    def __init__(self, longitude, latitude):
+        self.longitude = longitude
+        self.latitude = latitude
+        #self.get_wiki_pages()
+        #self.title = ""
+        #self.description = ""
+        #self.url = ""
+
+    def get_wiki_pages(self):
+
+        PAYLOAD = {"action": "query",
+                   "format": "json",
+                   "ggscoord": f"{self.longitude}|{self.latitude}",
+                   "generator": "geosearch",
+                   "prop": "extracts|info",
+                   "explaintext": True,  # convert HTML to plain text
+                   "exsentences": 3,  # how sentences to return
+                   "inprop": "url",  # get url (added info to prop for this)
+                   }
         # variable contains request result
         result = requests.get(WIKI_API_URL, PAYLOAD)
         #result = requests.get("https://fr.wikipedia.org/w/api.php?action=query&format=json&ggscoord=48.85658|2.35183&generator=geosearch&prop=extracts|info&explaintext&exsentences=3&inprop=url&titles=paris")
+
+        test = ""
 
         # if request works
         if result.status_code == 200:
             # mettre en json
             json_result = result.json()
-            # afficher résultat 200
-            #print(f"JSON RESULT: {json_result}")
 
             # ds dico query, le dico pages
             pages = json_result['query']['pages']  # access to pages from result json
-            #print(type(pages))  # dict
             pages = (list(pages.values()))  # convert values of pages into list of dict
-            #print(pages)
-            #print(type(pages))  # list
 
             # get title's value from first dict
             title = pages[0]['title']
-            print(f"TITLE: {title}")
 
             # get extract's value
-            extract = pages[0]['extract']
-            #print(type(extract))  # str
-            print(f"EXTRACT: {extract}")
+            description = pages[0]['extract']
 
             # get wikipedia link
             url = pages[0]['fullurl']
-            print(f"URL: {url}")
+
+            return title, description, url
 
         else:
-            print(f"Une erreur s'est produite: {result.status_code}")
+            #print(f"Une erreur s'est produite: {result.status_code}")
+            # error is a tuple with 3 elements, like result
+            err = (f"Une erreur s'est produite.", "Plus d'informations:", f"{result.status_code}")
+            return err
 
 
-wikiwrap = Wikiwrapper()
+#test
+wikiwrap = Wikiwrapper(48.85658, 2.35183)
 
+print(f"TITRE: {wikiwrap.get_wiki_pages()[0]}\n"
+      f"DESCRIPTION: {wikiwrap.get_wiki_pages()[1]}\n"
+      f"URL: {wikiwrap.get_wiki_pages()[2]}")
 
 """
 API_URL = "https://fr.wikipedia.org/w/api.php?"
