277461c105e39e5a9462229af19b2a5aded96594
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-12-18 16:29:56 +0100

    ♻️Refacto views

--- app/views.py
+++ app/views.py
@@ -1,8 +1,5 @@
 """
-Module that contains routes
-- routes: different URLs implemented by the app
-- view function : functions that will handle app routes
-- they are mapped to 1 or several URL routes so that Flask knows what to execute when a URL is requested
+Module that contains views of the app.
 """
 from flask import render_template, request
 
@@ -20,55 +17,32 @@ def index():
     return render_template('index.html', title='Accueil')
 
 
-@app.route('/result', methods=["POST"])  # route will be accessed by POST method
+@app.route('/result', methods=["POST"])  # route will be accessed by POSTmethod
 def result():
-    """Method result called when user submit his input that help us to retrieve data from JS to Python
+    """Method result called when user submit his input that help us
+    to retrieve data from JS to Python
     result view recieves an HTTP request and sends an HTTP response"""
 
     # object request allows me to access to data
     user_input = request.get_data('user_input').decode()  # decode unicode
-    print(user_input)
 
     # Get coordinates
     parser = Parser(user_input)
     geowrapper = GeoWrapper(parser.parsed_input)
 
+    # If one coord missing, stop here return error
+    if geowrapper.latitude is None or geowrapper.longitude is None:
+        return {"content": "Missing coordinates"}
+
     # If coordinates, get wiki infos
-    if (geowrapper.latitude, geowrapper.longitude) != (None, None):
-        wikiwrapper = Wikiwrapper()
-        wiki_details = wikiwrapper.get_wiki_info_by_long_lat(geowrapper.longitude, geowrapper.latitude)
-
-        if wiki_details is not None:
-            return {
-                "description": wiki_details['description'],
-                "url": wiki_details['url'],
-                "longitude": geowrapper.longitude,
-                "latitude": geowrapper.latitude,
-            }
-        else:
-            print("No wiki details found.")
-            return {"content": "error"}
+    wikiwrapper = Wikiwrapper()
+    wiki_details = wikiwrapper.get_wiki_info_by_long_lat(geowrapper.longitude, geowrapper.latitude)
+    if wiki_details is None:
+        return {"content": "No wiki details found"}
     else:
-        print("Wiki API needs coordinates.")
-        return {"content": "error"}
-
-
-
-
-
-"""
-Jinja2 remplace les {{ ... }} blocs par les valeurs correspondantes, 
-données par les arguments fournis dans l'appel render_template()
-
-jsonify fabrique du JSON et le met à l'intérieur d'une rep HTTP
-json.dumps fabrique du JSON tout court
-
-{{}}  : afficher qqchose
-{% %} : utiliser du python
-{# #} : intégrer un commentaire
-
-garder fonctions de views très courtes (8 lignes maxi):
-je récup l'info, je la traite en appelant fonction de traitement, je la renvoie
-fonction de traitement ds un autre fichier: utils.py
-
-"""
+        return {
+            "description": wiki_details['description'],
+            "url": wiki_details['url'],
+            "longitude": geowrapper.longitude,
+            "latitude": geowrapper.latitude,
+        }
