a18147d67c3be4d7bd38a8dcad6ccb3ee5a7d5cc
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-12-18 16:20:40 +0100

    🔧 Handling geo tokens and refacto config

--- app/geo_api.py
+++ app/geo_api.py
@@ -3,7 +3,7 @@ Module to get data from the Mapbox API
 """
 import requests
 
-from configuration.config import GEO_API_URL, TOKEN
+from configuration.config import GEO_API_URL, GEO_TOKEN
 
 
 class GeoWrapper:
@@ -15,16 +15,21 @@ class GeoWrapper:
         self.get_coordinates(input)
 
     def get_coordinates(self, input):
-        """Gets the coordinates from Mapbox API and sets latitude & longitude according to input"""
-        result = requests.get(f"{GEO_API_URL}{input}.json?access_token={TOKEN}&language=fr")
+        """Gets the coordinates from Mapbox API and sets
+        latitude & longitude according to input"""
+        result = requests.get(
+            f"{GEO_API_URL}{input}.json?access_token={GEO_TOKEN}&language=fr")
 
         if result.status_code == 200:
-            json_result = result.json()  # convert to Json
+            json_result = result.json()  # convert2json
 
-            # choose latitude & longitude from result and store them into variables
-            self.longitude = json_result['features'][0]['geometry']['coordinates'][1]
-            self.latitude = json_result['features'][0]['geometry']['coordinates'][0]
-            print(f"Pour {input} la longitude est {self.longitude} et la latitude est {self.latitude}")
+            # choose lat & long from result and store them into variables
+            self.longitude = \
+                json_result['features'][0]['geometry']['coordinates'][1]
+            self.latitude = \
+                json_result['features'][0]['geometry']['coordinates'][0]
+            print(f"Pour {input}, la longitude est {self.longitude} "
+                  f"et la latitude est {self.latitude}")
 
         else:
             print(f"Geo API error occured: {result.status_code}")
